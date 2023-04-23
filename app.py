b02e7d7ef10ef026c3d89a2d4099312778450b1a
angelyou370 <130410836+angelyou370@users.noreply.github.com>
2023-04-22 21:22:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import streamlit as st
 #def main():
 st.write('Welcome to Trivia! Pick your category:')
 #while true:
-topic_option = st.selectbox('Category',('Art History','Harvard', 'Sports'))
+topic_option = st.selectbox('Category',('Pick a Category','Art History','Harvard', 'Sports'))
 st.write('You selected:', topic_option)
 if topic_option == 'Art History':
    open(art_questions.csv) 
